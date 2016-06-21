db0d30614406327d69ec484c79b4f8abeaf12738
Rudelvi <rudelvi1096@gmail.com>
2016-06-20 22:33:46 -0400

    Implementing the method to list the book and request the last of them

--- src/Element.cpp
+++ src/Element.cpp
@@ -7,6 +7,6 @@
 
 #include "Element.h"
 
-Element::Element(std::string name):_name(name), _next(NULL)
+Element::Element(std::string name,  std::string author, std::string publisher, std::string date, std::string category):_name(name), _author(author), _publisher(publisher), _date(date), _category(category), _next(NULL
 {
 }
--- src/Stack.h
+++ src/Stack.h
@@ -17,6 +17,13 @@ class Stack
 		void push(Element*);
 		Element* pop();
 		Stack();
+
+		 Element* getFirst()
+			 {		return _first;	}
+
+			 Element* getLast()
+			 {		return _last;	}
+
 };
 
 #endif /* STACK_H_ */
